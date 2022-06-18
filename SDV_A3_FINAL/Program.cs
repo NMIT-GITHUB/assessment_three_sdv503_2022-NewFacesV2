7bec207b19bfcde9b7052ee4867345d8549f691f
Iain Henderson <99843389+NewFacesV2@users.noreply.github.com>
2022-06-19 11:02:30 +1200

    Update Program.cs

--- SDV_A3_FINAL/Program.cs
+++ SDV_A3_FINAL/Program.cs
@@ -1,22 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-// Declare variables and then initialize to zero.
+// Declare the two variables for the numbers and set to value of 0
 int num1 = 0; int num2 = 0;
 
-// Display title as the C# console calculator app.
+// Writelines for the calculator and explaining what it will do.
 Console.WriteLine("Welcome to Console Calculator in C#!\r");
 Console.WriteLine("----------------------------------------------------------------------------\n");
 Console.WriteLine("This program will take 2 seperate numbers and perform the chosen math function");
 
-// Ask the user to type the first number.
+
 Console.WriteLine("Please enter the first number, and then press Enter");
+
+// Setting value to num1 to the value entered by user
 num1 = Convert.ToInt32(Console.ReadLine());
 
-// Ask the user to type the second number.
 Console.WriteLine("Please enter the second number, and then press Enter");
+
+// Setting value to num2 to the second value entered
 num2 = Convert.ToInt32(Console.ReadLine());
 
-// Ask the user to choose an option.
+// Writelines to display each mathematical operator choice, using \t for neat indentation, I assigned a letter to each mathematical operator.
 Console.WriteLine("Choose an option from the following list:");
 Console.WriteLine("\ta - Add");
 Console.WriteLine("\ts - Subtract");
@@ -26,7 +29,9 @@ Console.WriteLine("\tr - Remainder");
 Console.WriteLine("\tp - Power");
 Console.Write("Your option? ");
 
-// Use a switch statement to do the math.
+/* Switch containing each math operator as a seperate case, with different background and foreground for each one.
+The default switch is for cases where a letter not on the list is pressed.
+*/
 switch (Console.ReadLine())
 {
     case "a":
@@ -65,6 +70,6 @@ switch (Console.ReadLine())
         Console.WriteLine("Invalid Choice, please enter a character from the following --- a, s, m, d, r, p");
         break;
 }
-// Wait for the user to respond before closing.
+// After the math is performed it will ask for any key to be pressed, Console.ReadKey() not asking for a particular key, will perform when any key is pressed.
 Console.Write("Press any key to close the Calculator console app");
 Console.ReadKey();
